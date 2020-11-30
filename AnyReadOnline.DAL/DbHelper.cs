539a671d3a6acbd7b2caaecb57c4e62b24e906c1
Granit Krasniqi <granit.krasniqi1@riinvest.net>
2020-11-30 02:39:48 +0100

    DbHelper

---
+++ AnyReadOnline.DAL/DbHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace AnyReadOnline.DAL
+{
+    class DbHelper
+    {
+        private static string ConnectionString = ConfigurationManager.ConnectionStrings["DbAnyReadConnection"].ConnectionString;
+
+        public static SqlConnection GetConnection()
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(ConnectionString);
+                conn.Open();
+                return conn;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot connect to Data Base server, please contact your administrator", "ERROR!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+
+        public static SqlCommand Command(SqlConnection connection, string cmdText, CommandType commandType)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand(cmdText, connection);
+                command.CommandType = commandType;
+                return command;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+
+
+
+    }
+}
+
