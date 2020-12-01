dfaf8d30ec3f1ec2c1c693a1d11c88e41596e89f
Endrit Tmava <endrit.tmava@riinvest.net>
2020-12-01 20:10:21 +0100

    Clients controller added

---
+++ AnyReadOnline/Controllers/ClientController.cs
@@ -0,0 +1,144 @@
+﻿using ANYREAD.Models;
+using AnyReadOnline.Models;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnyReadOnline.Controllers
+{
+    public class ClientController : Controller
+    {
+
+
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public ClientController()
+        {
+        }
+
+        public ClientController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        // GET: Client
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Client/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Client/Create
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+
+        // POST: Client/Create
+        [HttpPost]
+        public async Task<ActionResult> Register(Client client)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var user = new ApplicationUser { UserName = client.UserName, Email = client.Email };
+                    var result = await UserManager.CreateAsync(user, client.Password);
+                    if (result.Succeeded)
+                    {
+                        await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
+                        return RedirectToAction("Index", "Home");
+                    }
+                    
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Client/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Client/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Client/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Client/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- AnyReadOnline/Controllers/HomeController.cs
+++ AnyReadOnline/Controllers/HomeController.cs
@@ -12,6 +12,10 @@ namespace AnyReadOnline.Controllers
         {
             return View();
         }
+        public ActionResult Test()
+        {
+            return View();
+        }
 
         public ActionResult About()
         {
