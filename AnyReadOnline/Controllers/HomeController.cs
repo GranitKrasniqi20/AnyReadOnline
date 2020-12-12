69ea23090dd19a252ad90612cf07ee0039f95bb1
Beis Presheva <beis.presheva@riinvest.net>
2020-12-12 23:28:55 +0100

    small change

--- AnyReadOnline/Controllers/HomeController.cs
+++ AnyReadOnline/Controllers/HomeController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AnyReadOnline.BOL;
+using AnyReadOnline.BLL;
 
 namespace AnyReadOnline.Controllers
 {
     public class HomeController : Controller
     {
+        Book book = new Book();
+        BookBLL bookBLL = new BookBLL();
+
         public ActionResult Index()
         {
             return View();
@@ -28,6 +33,11 @@ namespace AnyReadOnline.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Searching(string text)
+        {
             return View();
         }
     }
