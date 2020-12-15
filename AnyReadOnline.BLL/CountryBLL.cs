f012dbe6d233527ffe1ccf751fc5ff293dfbd9ee
Endrit Tmava <endrit.tmava@riinvest.net>
2020-12-15 10:50:53 +0100

    bug fixes, staff bll added, staff dal updated, register fixed, db script added.

---
+++ AnyReadOnline.BLL/CountryBLL.cs
@@ -0,0 +1,44 @@
+﻿using AnyReadOnline.BOL;
+using AnyReadOnline.BOL.Interfaces;
+using AnyReadOnline.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyReadOnline.BLL
+{
+    public class CountryBLL : ICrud<Country>
+    {
+        private readonly CountryDAL CountryDAL = new CountryDAL();
+
+        public int Add(Country obj)
+        {
+            return CountryDAL.Add(obj);
+        }
+
+        public int Delete(int id)
+        {
+            return CountryDAL.Delete(id);
+        }
+
+        public Country Get(int id)
+        {
+            return CountryDAL.Get(id);
+        }
+
+        public List<Country> GetAll()
+        {
+            return CountryDAL.GetAll();
+        }
+
+        public int Update(Country obj)
+        {
+            return CountryDAL.Update(obj);
+        }
+
+
+
+    }
+}
