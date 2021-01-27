6fc1e4527c267ed869e6f5133ada98bffdf354b5
Granit Krasniqi <granit.krasniqi1@riinvest.net>
2021-01-27 15:16:38 +0100

    Editing GK

---
+++ AnyReadOnline/Controllers/MultiLanguagesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnyReadOnline.Controllers
+{
+    public class MultiLanguagesController : Controller
+    {
+        // GET: MultiLanguages
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Change(string lngName)
+        {
+            if(lngName != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(lngName);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(lngName);
+            }
+
+            HttpCookie cookie = new HttpCookie("LanguageCookie"); //Language nje emer ku ruhen variablat
+            cookie.Value = lngName;
+            Response.Cookies.Add(cookie);
+
+            return View("Index");
+        }
+    }
+}
